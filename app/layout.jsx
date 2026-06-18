6d47a8e391fab262cc41e5d236fda830b7673e7d
sallywe88-hue <sallywe88@gmail.com>
2026-06-19 00:57:21 +0900

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Progress Widget",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
